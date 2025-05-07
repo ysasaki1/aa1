8d70ddcc7a61631d7666b8a7fae6b4c7044f31e0
ysasaki1 <ysasaki1@hawk.iit.edu>
2025-05-07 18:58:39 +0900

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,33 +1,102 @@
-#Types
-#The Terraform language uses the following types for its values:
+# Variable definitions with types
 
-# https://developer.hashicorp.com/terraform/language/expressions/types
-# string: a sequence of Unicode characters representing some text, like "hello".
-# number: a numeric value. The number type can represent both whole numbers like 15 and fractional values like 6.283185.
-# bool: a boolean value, either true or false. bool values can be used in conditional logic.
-# list (or tuple): a sequence of values, like ["us-west-1a", "us-west-1c"]. Identify elements in a list with consecutive whole numbers, starting with zero.
-# set: a collection of unique values that do not have any secondary identifiers or ordering.
-# map (or object): a group of values identified by named labels, like {name = "Mabel", age = 52}.
+variable "imageid" {
+  description = "The ID of the AMI to use for the instance"
+  type        = string
+}
 
-# Default types are stings, lists, and maps
+variable "instance-type" {
+  description = "The type of instance to create"
+  type        = string
+}
 
-variable "imageid" {}
-variable "instance-type" {}
-variable "key-name" {}
-variable "vpc_security_group_ids" {}
-variable "cnt" {}
-variable "az" { default = ["us-east-2a", "us-east-2b", "us-east-2c"] }
-variable "tag-name" {}
-variable "raw-bucket" {}
-variable "finished-bucket" {}
-variable "sns-topic" {}
-variable "sqs" {}
-variable "dbname" {}
-variable "uname" {}
-variable "pass" {}
-variable "elb-name" {}
-variable "asg-name" {}
-variable "min" {}
-variable "max" {}
-variable "desired" {}
-variable "tg-name" {}
+variable "key-name" {
+  description = "The name of the key pair to use for SSH access"
+  type        = string
+}
+
+variable "vpc_security_group_ids" {
+  description = "List of VPC security group IDs to associate with the instance"
+  type        = list(string)
+}
+
+variable "cnt" {
+  description = "Count of instances to create"
+  type        = number
+}
+
+variable "az" {
+  description = "List of availability zones"
+  type        = list(string)
+  default     = ["us-east-2a", "us-east-2b", "us-east-2c"]
+}
+
+variable "tag-name" {
+  description = "The tag name for resources"
+  type        = string
+}
+
+variable "raw-bucket" {
+  description = "Name of the raw S3 bucket"
+  type        = string
+}
+
+variable "finished-bucket" {
+  description = "Name of the finished S3 bucket"
+  type        = string
+}
+
+variable "sns-topic" {
+  description = "Name of the SNS topic"
+  type        = string
+}
+
+variable "sqs" {
+  description = "Name of the SQS queue"
+  type        = string
+}
+
+variable "dbname" {
+  description = "Name of the RDS database"
+  type        = string
+}
+
+variable "uname" {
+  description = "Username for the RDS database"
+  type        = string
+}
+
+variable "pass" {
+  description = "Password for the RDS database"
+  type        = string
+}
+
+variable "elb-name" {
+  description = "Name of the Elastic Load Balancer"
+  type        = string
+}
+
+variable "asg-name" {
+  description = "Name of the Auto Scaling Group"
+  type        = string
+}
+
+variable "min" {
+  description = "Minimum number of instances in the Auto Scaling Group"
+  type        = number
+}
+
+variable "max" {
+  description = "Maximum number of instances in the Auto Scaling Group"
+  type        = number
+}
+
+variable "desired" {
+  description = "Desired number of instances in the Auto Scaling Group"
+  type        = number
+}
+
+variable "tg-name" {
+  description = "Name of the Load Balancer Target Group"
+  type        = string
+}
